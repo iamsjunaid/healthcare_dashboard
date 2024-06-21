250b26f4c860eac20982b66d72453d1114ce2630
Junaid Syed <sjunaid626@gmail.com>
2024-06-21 10:36:33 +0530

    refactor: Update script.js to fetch patient data using encoded credentials on page load

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ let patients_data = [];
 window.onload = async function () {
   await getPatientData();
 };
+
+async function getPatientData() {
+  await fetch("https://fedskillstest.coalitiontechnologies.workers.dev", {
+    method: "GET",
+    headers: {
+      Authorization: `Basic ${encodedCredentials}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const jessicaData = data.find(
+        (patient) => patient.name === "Jessica Taylor"
+      );
+      if (jessicaData) {
+        setData(jessicaData);
+        createBloodPressureChart(jessicaData);
+        setBPCount(jessicaData);
+        setHealthStatus(jessicaData);
+        setDiagnosticList(jessicaData);
+        setPatientInfo(jessicaData);
+        setLabResults(jessicaData);
+      } else {
+        console.error("Jessica Taylor not found");
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
