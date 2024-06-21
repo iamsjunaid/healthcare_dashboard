dba8ef2f79aaf05d46cf76b03e87f87b20b63082
Junaid Syed <sjunaid626@gmail.com>
2024-06-21 10:35:00 +0530

    style: Use credentials from the CONFIG object in script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // script.js
 import CONFIG from "./config.js";
 
+// Use credentials from the CONFIG object
+const username = CONFIG.username;
+const password = CONFIG.password;
+const credentials = `${username}:${password}`;
